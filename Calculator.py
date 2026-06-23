40c33db5b101218e76637d7758622284f9cf5495
Subithra-R <24bca70@cttewc.edu.in>
2026-06-23 07:38:38 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+# Simple Calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operator = input("Enter operation (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+    print("Result =", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("Result =", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result =", result)
+
+elif operator == "/":
+    if num2 == 0:
+        print("Error! Division by zero is not allowed.")
+    else:
+        result = num1 / num2
+        print("Result =", result)
+
+else:
+    print("Invalid operator!")
